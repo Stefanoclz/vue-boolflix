12c8db5adbffa164f7753361c3a161477bdc182a
Stefano Calzoni <stefanoclz@hotmail.it>
2022-04-20 12:09:36 +0200

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h2>BOOLFLIX</h2>
+    <input type="text" v-model="searchText" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "headerComponent",
+  data() {
+    return {
+      searchText: "",
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+header {
+  background-color: black;
+  display: flex;
+  justify-content: space-between;
+  padding: 30px;
+
+  h2 {
+    color: red;
+  }
+}
+</style>
